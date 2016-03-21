25703ac678e52ea0d60001cd0d00c0808b3fc370
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-21 18:24:07 -0400

    clientele page laid out somewhat decently

--- page-clientele.php
+++ page-clientele.php
@@ -2,11 +2,53 @@
   get_header();
   $thumb = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 ?>
-<header class="splash flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
-  <h1><?php echo stripslashes( get_option( 'frontpage_splash_title', 'Splash Title Here' ) ); ?></h1>
-  <h3><?php echo stripslashes( get_option( 'frontpage_splash_tagline', 'Witty splash tagline should go here' ) ); ?></h3>
-  <a class="ghost smooth-scroll" href="#main">Grow With Us</a>
-</header>
-  <h2>Clientele</h2>
+  <header class="splash flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
+    <h1><?php echo stripslashes( get_option( 'frontpage_splash_title', 'Splash Title Here' ) ); ?></h1>
+    <h3><?php echo stripslashes( get_option( 'frontpage_splash_tagline', 'Witty splash tagline should go here' ) ); ?></h3>
+  </header>
+
+<main id="main">
+  <section id="clientele" class="text-center">
+    <h2>Our Clients</h2>
+    <div class="container-md clearfix">
+    <?php
+      // Display Clientele
+      $args = array( 'post_type' => 'client', 'posts_per_page' => 5 );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+        echo '<div class="client">';
+          echo '<img src="' .  wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) . '" title="' . get_the_title() . '" alt="' . get_the_title() . '">';
+        echo '</div>';
+      endwhile;
+    ?>
+    </div>
+  </section>
+
+  <section id="testimonials" class="text-center">
+    <h2>What Our Clients Say About Us</h2>
+    <div class="container-lg clearfix">
+    <?php
+      // Display Testimonials
+      $args = array( 'post_type' => 'testimonial', 'posts_per_page' => 5 );
+      $loop = new WP_Query( $args );
+      while ( $loop->have_posts() ) : $loop->the_post();
+        echo '<div class="testimonial clearfix">';
+          echo '<div class="row">';
+            echo '<div class="col-half">';
+              echo '<img src="' .  wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) . '">';
+            echo '</div>';
+            echo '<div class="col-half">';
+              echo '<h3>' . get_the_title() . '</h3>';
+              echo '<blockquote>' . get_post_meta($post->ID, 'testimonial_quote', true) . '</blockquote>';
+            echo '</div>';
+          echo '</div>';
+        echo '</div>';
+      endwhile;
+    ?>
+    </div>
+  </section>
+</main>
+
 <?php
+
   get_footer();
