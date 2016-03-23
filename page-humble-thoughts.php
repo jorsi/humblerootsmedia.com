aae38b656173acfbc1986a8944e6d78785af3bc0
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-23 14:15:36 -0400

    humble thoughts formatting

--- page-humble-thoughts.php
+++ page-humble-thoughts.php
@@ -2,24 +2,25 @@
   get_header();
   $thumb = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 ?>
-  <div class="nav-pad"></div>
-<header class="splash-half flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
-  <h1><?php echo stripslashes( get_option( 'humblethoughts_splash_title', 'Our Humble Thoughts' ) ); ?></h1>
+
+<div class="nav-pad"></div>
+<header class="flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
+  <h1 class="tangerine"><?php echo stripslashes( get_option( 'humblethoughts_splash_title', 'Our Humble Thoughts' ) ); ?></h1>
   <h3><?php echo stripslashes( get_option( 'humblethoughts_splash_tagline' ) ); ?></h3>
 </header>
+
 <main id="main">
   <section class="blog-posts">
-    <div class="container-md">
-
-      <!-- Start the Loop. -->
-      <?php
-        query_posts(null);
-        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <article class="post-summary">
-          	<h2 class="post-title">
+    <!-- Start the Loop. -->
+    <?php
+      query_posts(null);
+      if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <section class="post-summary">
+          <div class="container-md">
+          	<h2 class="post-title text-center">
               <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
             </h2>
-            <p class="post-metadata">
+            <p class="post-metadata text-center">
               Posted in <?php the_category( ', ' ); ?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
             </p>
 
@@ -28,13 +29,12 @@
           	<article class="post-entry">
           		<?php the_content(); ?>
           	</article>
-          </article>
+          </div>
+        </section>
 
-        <?php endwhile; else : ?>
-      	   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-      <?php endif; ?>
-
-    </div>
+      <?php endwhile; else : ?>
+    	   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
   </section>
 </main>
 
