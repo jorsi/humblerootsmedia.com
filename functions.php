15de8f5a24dc31a04aa3d8019c56ecb3908ef7e9
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-23 16:03:41 -0400

    increased upload limit

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 
 // Require the custom options page for Humble Roots Media
 require_once( 'inc/humblerootsmedia-options.php' );
