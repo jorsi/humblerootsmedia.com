fe50bc6ab5cbf48a1ed3e66e2550c70f8f02d7b2
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-23 15:46:22 -0400

    contact page begun

--- page-contact.php
+++ page-contact.php
@@ -3,11 +3,41 @@
   $thumb = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 ?>
   <div class="nav-pad"></div>
-<header class="splash flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
-  <h1><?php echo stripslashes( get_option( 'frontpage_splash_title', 'Splash Title Here' ) ); ?></h1>
-  <h3><?php echo stripslashes( get_option( 'frontpage_splash_tagline', 'Witty splash tagline should go here' ) ); ?></h3>
-  <a class="ghost smooth-scroll" href="#main">Grow With Us</a>
-</header>
-  <h2>Contact</h2>
+<main id="main">
+  <aside class="break text-center">
+    <div class="container-md">
+      <p><?php echo stripslashes( get_option( 'contact_intro', 'You are only a few moments away from creating beautiful content for your next major project.' ) ); ?></p>
+    </div>
+  </aside>
+
+  <section class="content contact-form">
+    <div class="container-lg">
+      <form method="post">
+        <input type="hidden" name="action" value="contact">
+        <label>
+          <input type="text" onfocus="if(this.value == this.defaultValue) this.value = ''" onblur="if(this.value == '') this.value = this.defaultValue" name="name.full" value="Your Name">
+        </label>
+        <label>
+          <input type="email" onfocus="if(this.value == this.defaultValue) this.value = ''" onblur="if(this.value == '') this.value = this.defaultValue" value="Your email" name="email">
+        </label>
+        <label>
+          <input type="text" onfocus="if(this.value == this.defaultValue) this.value = ''" onblur="if(this.value == '') this.value = this.defaultValue" value="Your contact number" name="phone">
+        </label>
+        <label>
+          <select name="enquiryType">
+            <option selected disabled>What are you contacting us about?</option>
+            <option value="message">Just leaving a message</option>
+            <option value="question">I've got a question</option>
+            <option value="other">Something else...</option>
+          </select>
+        </label>
+        <label>
+          <textarea name="message" onfocus="if(this.value == this.defaultValue) this.value = ''" onblur="if(this.value == '') this.value = this.defaultValue">Tell us a bit about yourself</textarea>
+        </label>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </section>
+</main>
 <?php
   get_footer();
