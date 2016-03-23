2fabd3b9dc59551a5325162cbb2508c39aa034f4
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-23 14:23:26 -0400

    formatting consistent with humble thoughts page

--- single.php
+++ single.php
@@ -6,14 +6,14 @@
 ?>
 <div class="nav-pad"></div>
 <header class="splash flex flex-column flex-center text-center parallax-window" data-parallax="scroll" data-image-src="<?php echo $thumb; ?>">
-  <h1><?php single_post_title(); ?></h1>
+  <h1 class="post-title tangerine"><?php single_post_title(); ?></h1>
+  <h2 class="post-metadata text-center">
+    Posted in <?php the_category( ', ' ); ?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
+  </h2>
 </header>
 <main id="main">
   <section class="post-content">
     <div class="container-md">
-      <h3 class="post-metadata text-center">
-        Posted in <?php the_category( ', ' ); ?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
-      </h3>
       <article class="post-entry">
         <?php the_content(); ?>
       </article>
