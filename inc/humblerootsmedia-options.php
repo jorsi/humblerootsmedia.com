3c792cb10c88abfc824e3e5afeddd46ef86e41bf
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-03-24 15:44:27 -0400

    added fancybox for video modals

--- inc/humblerootsmedia-options.php
+++ inc/humblerootsmedia-options.php
@@ -91,6 +91,7 @@ function humblerootsmedia_init() {
   add_action( 'wp_enqueue_scripts', 'humblerootsmedia_enqueue' );
   function humblerootsmedia_enqueue() {
 
+      // Global Styles
       wp_enqueue_style( 'humblerootsmedia_style', get_stylesheet_uri() );
       wp_enqueue_style( 'humblerootsmedia_style_normalize', get_template_directory_uri().'/styles/normalize.css' );
       wp_enqueue_style( 'humblerootsmedia_style_googlefont_lato', 'https://fonts.googleapis.com/css?family=Lato:400,300,700,900' );
@@ -103,8 +104,7 @@ function humblerootsmedia_init() {
       wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", false, null);
       wp_enqueue_script('jquery');
 
-      wp_enqueue_script( 'humblerootsmedia_script_instafeed', get_template_directory_uri().'/js/instafeed.min.js', array('jquery'));
-      wp_enqueue_script( 'humblerootsmedia_script_bxslider', get_template_directory_uri().'/js/jquery.bxslider.min.js', array('jquery'));
+      // Global scripts
       wp_enqueue_script( 'humblerootsmedia_script_parallax', get_template_directory_uri().'/js/jquery.parallax.min.js', array('jquery'));
       wp_enqueue_script( 'humblerootsmedia_script_smoothscroll', get_template_directory_uri() . '/js/jquery.smooth-scroll.min.js', array('jquery'));
       wp_enqueue_script( 'humblerootsmedia_script_main', get_template_directory_uri().'/js/main.js', array('jquery'));
@@ -112,6 +112,12 @@ function humblerootsmedia_init() {
       /**
        * Adds Page specific styles and scripts
        */
+      if ( is_front_page() ) {
+        wp_enqueue_style( 'humblerootsmedia_style_fancybox', get_stylesheet_directory_uri() . '/styles/jquery.fancybox.css');
+        wp_enqueue_script( 'humblerootsmedia_script_fancybox', get_template_directory_uri().'/js/jquery.fancybox.pack.js', array('jquery'));
+        wp_enqueue_script( 'humblerootsmedia_script_bxslider', get_template_directory_uri().'/js/jquery.bxslider.min.js', array('jquery'));
+        wp_enqueue_script( 'humblerootsmedia_script_instafeed', get_template_directory_uri().'/js/instafeed.min.js', array('jquery'));
+      }
       if ( is_page( 'Clientele') )
         wp_enqueue_style( 'humblerootsmedia_style_clientele', get_stylesheet_directory_uri() . '/styles/clientele.css');
       if ( is_page( 'Humble Thoughts') )
