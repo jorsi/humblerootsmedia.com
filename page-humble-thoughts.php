f0f348bc4ef10aa454bc88cb67f9665d11ba91af
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-04-03 16:30:41 -0400

    humble thoughts posts now in container

--- page-humble-thoughts.php
+++ page-humble-thoughts.php
@@ -20,39 +20,40 @@
 </header>
 
 <main id="main">
-  <section class="blog-posts">
-    <!-- Start the Loop. -->
-    <?php
-      query_posts(null);
-      if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <section class="post-summary">
-          <div class="container-md">
-          	<h2 class="post-title text-center">
-              <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-            </h2>
-            <p class="post-metadata text-center">
-              Posted in <?php the_category( ', ' ); ?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
-            </p>
+  <div class="container-lg">
+    <section class="blog-posts">
+      <!-- Start the Loop. -->
+      <?php
+        query_posts(null);
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <section class="post-summary">
+            <div class="container-md">
+            	<h2 class="post-title text-center">
+                <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+              </h2>
+              <p class="post-metadata text-center">
+                Posted in <?php the_category( ', ' ); ?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?>
+              </p>
 
-            <img class="post-image block-center" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>">
+              <img class="post-image block-center" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>">
 
-          	<article class="post-entry">
-          		<?php the_content(); ?>
-          	</article>
-          </div>
-        </section>
+            	<article class="post-entry">
+            		<?php the_content(); ?>
+            	</article>
+            </div>
+          </section>
 
-      <?php endwhile; else : ?>
-    	   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-    <?php endif; ?>
-  </section>
+        <?php endwhile; else : ?>
+      	   <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
+    </section>
 
     <aside class="break text-center">
       <div class="container-md">
         <p><?php echo stripslashes( get_post_meta($postid, 'humblerootsmedia_outro-text', true ) ); ?></p>
       </div>
-
     </aside>
+  </div>
 </main>
 
 <?php
