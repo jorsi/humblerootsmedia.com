724860e5dfa8bb1b5ad9c534676454537aadac6e
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-04-03 13:12:18 -0400

    registered footer custom post

--- inc/humblerootsmedia-customposts.php
+++ inc/humblerootsmedia-customposts.php
@@ -1,5 +1,4 @@
 <?php
-
 // Custom Post Types
 function humblerootsmedia_custom_posts() {
   $labels = array(
@@ -27,73 +26,6 @@ function humblerootsmedia_custom_posts() {
     'register_meta_box_cb' => 'add_productions_metaboxes',
     'has_archive'   => true,
   );
-  // Adds the Testimonial Custom Fields
-  function add_productions_metaboxes() {
-    add_meta_box('productions_video_description', 'Video Description', 'productions_video_desc_display', 'production', 'normal', 'default');
-      add_meta_box('productions_video_uri', 'Video Source', 'productions_video_uri_display', 'production', 'normal', 'default');
-  }
-  // The Production Metaboxes
-  function productions_video_desc_display() {
-  	global $post;
-
-  	// Noncename needed to verify where the data originated
-  	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
-  	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
-
-  	// Get the location data if its already been entered
-  	$location = get_post_meta($post->ID, 'productions_video_description', true);
-
-  	// Echo out the field
-  	echo '<input type="text" name="productions_video_description" value="' . $location  . '" class="widefat" />';
-  }
-  function productions_video_uri_display() {
-  	global $post;
-
-  	// Noncename needed to verify where the data originated
-  	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
-  	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
-
-  	// Get the location data if its already been entered
-  	$location = get_post_meta($post->ID, 'productions_video_uri', true);
-
-  	// Echo out the field
-  	echo '<input type="text" name="productions_video_uri" value="' . $location  . '" class="widefat" />';
-  }
-
-  // Save the Metabox Data
-  function productions_save_data($post_id, $post) {
-
-  	// verify this came from the our screen and with proper authorization,
-  	// because save_post can be triggered at other times
-  	if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
-  	   return $post->ID;
-  	}
-
-  	// Is the user allowed to edit the post or page?
-  	if ( !current_user_can( 'edit_post', $post->ID ))
-  		return $post->ID;
-
-  	// OK, we're authenticated: we need to find and save the data
-  	// We'll put it into an array to make it easier to loop though.
-
-  	$events_meta['productions_video_uri'] = $_POST['productions_video_uri'];
-    $events_meta['productions_video_description'] = $_POST['productions_video_description'];
-
-  	// Add values of $events_meta as custom fields
-
-  	foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
-  		if( $post->post_type == 'revision' ) return; // Don't store custom data twice
-  		$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
-  		if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
-  			update_post_meta($post->ID, $key, $value);
-  		} else { // If the custom field doesn't have a value
-  			add_post_meta($post->ID, $key, $value);
-  		}
-  		if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
-  	}
-  }
-  add_action('save_post', 'productions_save_data', 1, 2); // save the custom fields
-
   register_post_type( 'production', $args );
 
   $labels = array(
@@ -147,59 +79,156 @@ function humblerootsmedia_custom_posts() {
     'register_meta_box_cb' => 'add_testimonial_metaboxes',
     'has_archive'   => true,
   );
-  // Adds the Testimonial Custom Fields
-  function add_testimonial_metaboxes() {
-      add_meta_box('testimonial_quote', 'Testimonial Quote', 'testimonial_quote_display', 'testimonial', 'normal', 'default');
-  }
-  // The Testimonial Quote Metabox
-  function testimonial_quote_display() {
-  	global $post;
-
-  	// Noncename needed to verify where the data originated
-  	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
-  	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
-
-  	// Get the location data if its already been entered
-  	$location = get_post_meta($post->ID, 'testimonial_quote', true);
-
-  	// Echo out the field
-  	echo '<input type="text" name="testimonial_quote" value="' . $location  . '" class="widefat" />';
-  }
-    // Save the Metabox Data
-
-  function testimonial_save_data($post_id, $post) {
-
-  	// verify this came from the our screen and with proper authorization,
-  	// because save_post can be triggered at other times
-  	if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
-  	return $post->ID;
-  	}
-
-  	// Is the user allowed to edit the post or page?
-  	if ( !current_user_can( 'edit_post', $post->ID ))
-  		return $post->ID;
-
-  	// OK, we're authenticated: we need to find and save the data
-  	// We'll put it into an array to make it easier to loop though.
-
-  	$events_meta['testimonial_quote'] = $_POST['testimonial_quote'];
-
-  	// Add values of $events_meta as custom fields
-
-  	foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
-  		if( $post->post_type == 'revision' ) return; // Don't store custom data twice
-  		$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
-  		if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
-  			update_post_meta($post->ID, $key, $value);
-  		} else { // If the custom field doesn't have a value
-  			add_post_meta($post->ID, $key, $value);
-  		}
-  		if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
-  	}
-
-  }
-  add_action('save_post', 'testimonial_save_data', 1, 2); // save the custom fields
-
   register_post_type( 'testimonial', $args );
+
+  $labels = array(
+    'name'               => _x( 'Footer', 'post type general name' ),
+    // 'singular_name'      => _x( 'Footer', 'post type singular name' ),
+    // 'add_new'            => _x( 'Add New', 'book' ),
+    // 'add_new_item'       => __( 'Add New Production' ),
+    'edit_item'          => __( 'Edit Production' ),
+    // 'new_item'           => __( 'New Production' ),
+    'all_items'          => __( 'All Footer Items' ),
+    'view_item'          => __( 'View Footer Item' ),
+    // 'search_items'       => __( 'Search Productions' ),
+    'not_found'          => __( 'No productions found' ),
+    'not_found_in_trash' => __( 'No productions found in the Trash' ),
+    'parent_item_colon'  => '',
+    'menu_name'          => 'Footer'
+  );
+  $args = array(
+    'labels'        => $labels,
+    'description'   => 'Sections for the footer',
+    'public'        => true,
+    'menu_position' => 5,
+    'menu_icon'     => 'dashicons-video-alt',
+    'supports'      => array( 'title', 'thumbnail', 'page-attributes' ),
+    'has_archive'   => true,
+    'capability_type' => 'post',
+    'capabilities' => array(
+      'create_posts' => 'do_not_allow'
+    ),
+    'map_meta_cap' => true, // Set to `false`, if users are not allowed to edit/delete existing posts
+  );
+  register_post_type( 'footer', $args);
 }
 add_action( 'init', 'humblerootsmedia_custom_posts' );
+
+// Adds the Testimonial Custom Fields
+function add_productions_metaboxes() {
+  add_meta_box('productions_video_description', 'Video Description', 'productions_video_desc_display', 'production', 'normal', 'default');
+    add_meta_box('productions_video_uri', 'Video Source', 'productions_video_uri_display', 'production', 'normal', 'default');
+}
+// The Production Metaboxes
+function productions_video_desc_display() {
+  global $post;
+
+  // Noncename needed to verify where the data originated
+  echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
+  wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+  // Get the location data if its already been entered
+  $location = get_post_meta($post->ID, 'productions_video_description', true);
+
+  // Echo out the field
+  echo '<input type="text" name="productions_video_description" value="' . $location  . '" class="widefat" />';
+}
+function productions_video_uri_display() {
+  global $post;
+
+  // Noncename needed to verify where the data originated
+  echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
+  wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+  // Get the location data if its already been entered
+  $location = get_post_meta($post->ID, 'productions_video_uri', true);
+
+  // Echo out the field
+  echo '<input type="text" name="productions_video_uri" value="' . $location  . '" class="widefat" />';
+}
+
+// Save the Metabox Data
+function productions_save_data($post_id, $post) {
+
+  // verify this came from the our screen and with proper authorization,
+  // because save_post can be triggered at other times
+  if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
+     return $post->ID;
+  }
+
+  // Is the user allowed to edit the post or page?
+  if ( !current_user_can( 'edit_post', $post->ID ))
+    return $post->ID;
+
+  // OK, we're authenticated: we need to find and save the data
+  // We'll put it into an array to make it easier to loop though.
+
+  $events_meta['productions_video_uri'] = $_POST['productions_video_uri'];
+  $events_meta['productions_video_description'] = $_POST['productions_video_description'];
+
+  // Add values of $events_meta as custom fields
+
+  foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
+    if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+    $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+    if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+      update_post_meta($post->ID, $key, $value);
+    } else { // If the custom field doesn't have a value
+      add_post_meta($post->ID, $key, $value);
+    }
+    if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+  }
+}
+// Adds the Testimonial Custom Fields
+function add_testimonial_metaboxes() {
+    add_meta_box('testimonial_quote', 'Testimonial Quote', 'testimonial_quote_display', 'testimonial', 'normal', 'default');
+}
+// The Testimonial Quote Metabox
+function testimonial_quote_display() {
+  global $post;
+
+  // Noncename needed to verify where the data originated
+  echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' .
+  wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+  // Get the location data if its already been entered
+  $location = get_post_meta($post->ID, 'testimonial_quote', true);
+
+  // Echo out the field
+  echo '<input type="text" name="testimonial_quote" value="' . $location  . '" class="widefat" />';
+}
+  // Save the Metabox Data
+
+function testimonial_save_data($post_id, $post) {
+
+  // verify this came from the our screen and with proper authorization,
+  // because save_post can be triggered at other times
+  if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
+  return $post->ID;
+  }
+
+  // Is the user allowed to edit the post or page?
+  if ( !current_user_can( 'edit_post', $post->ID ))
+    return $post->ID;
+
+  // OK, we're authenticated: we need to find and save the data
+  // We'll put it into an array to make it easier to loop though.
+
+  $events_meta['testimonial_quote'] = $_POST['testimonial_quote'];
+
+  // Add values of $events_meta as custom fields
+
+  foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
+    if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+    $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+    if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+      update_post_meta($post->ID, $key, $value);
+    } else { // If the custom field doesn't have a value
+      add_post_meta($post->ID, $key, $value);
+    }
+    if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+  }
+}
+
+add_action('save_post', 'productions_save_data', 1, 2); // save the custom fields
+add_action('save_post', 'testimonial_save_data', 1, 2); // save the custom fields
