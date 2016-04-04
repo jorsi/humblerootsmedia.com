cb1630f909f71c74f7bc4ea182b83af345f102aa
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-04-04 19:05:45 -0400

    simple 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+  get_header();
+  $thumb = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+?>
+  <div class="nav-pad"></div>
+<main id="main">
+  <aside class="break text-center">
+    <div class="container-md">
+      <p>
+        Oops... it looks like we can't find the page you are looking for.
+      </p>
+      <p>
+        <a href="javascript:history.back()">Return from whence ye came</a>
+      </p>
+    </div>
+  </aside>
+</main>
+<?php
+  get_footer();
