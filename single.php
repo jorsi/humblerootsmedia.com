f5adda6950e0af776125e0b54df44fd432ad9962
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-05-04 12:26:40 -0400

    added recent posts to single post page

--- single.php
+++ single.php
@@ -30,6 +30,35 @@
       </article>
     </div>
   </section>
+
+  <section class="recent-posts">
+    <div class="container-md">
+      <h2 class="recent-posts_h2">Read On</h2>
+      <ul class="recent-posts_ul clearfix">
+      <?php
+        $args = array(
+          'numberposts' => 5,
+          'offset' => 0,
+          'category' => 0,
+          'orderby' => 'post_date',
+          'exclude' => $post->ID,
+          'order' => 'DESC',
+          'post_type' => 'post',
+          'post_status' => 'draft, publish, future, pending, private',
+          'suppress_filters' => true );
+      	$recent_posts = wp_get_recent_posts( $args );
+      	foreach( $recent_posts as $recent ){
+      		echo '<li class="recent-posts_li">';
+            echo '<a class="recent-posts_a" href="' . get_permalink($recent["ID"]) . '">';
+              echo '<img class="recent-posts_img" src="' . wp_get_attachment_image_src( get_post_thumbnail_id( $recent["ID"] ), 'featured-thumb' )[0] . '">';
+              echo '<h4 class="recent-posts_h4">' . $recent["post_title"] . '</h4>';
+            echo '</a>';
+          echo '</li>';
+      	}
+      ?>
+      </ul>
+    </div>
+  </section>
 </main>
 
 <?php
