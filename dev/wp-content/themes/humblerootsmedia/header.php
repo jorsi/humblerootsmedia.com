4d9fd2551eb96251b7cc7bc77ec0414e628306cb
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-05-27 11:39:48 -0400

    deleted meta description because yoast told me to...

--- dev/wp-content/themes/humblerootsmedia/header.php
+++ dev/wp-content/themes/humblerootsmedia/header.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
 	<meta name="author" content="<?php echo get_bloginfo( 'author' ); ?>">
 
 	<title><?php echo get_bloginfo(); ?></title>
