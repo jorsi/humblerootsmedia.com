2dfa4cda3aa3583bec3080b70aad7c37fdbe1a26
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-06-28 13:25:22 -0400

    fancybox gallery for post pages added

--- dev/wp-content/themes/humblerootsmedia/inc/humblerootsmedia-enqueue.php
+++ dev/wp-content/themes/humblerootsmedia/inc/humblerootsmedia-enqueue.php
@@ -9,9 +9,11 @@ function humblerootsmedia_enqueue() {
     wp_enqueue_style( 'humblerootsmedia_style_normalize', get_template_directory_uri().'/css/lib/normalize.css' );
     wp_enqueue_style( 'humblerootsmedia_style_googlefont_cabin', 'https://fonts.googleapis.com/css?family=Cabin' );
     wp_enqueue_style( 'humblerootsmedia_style_fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
+    wp_enqueue_style( 'humblerootsmedia_style_fancybox', get_stylesheet_directory_uri() . '/css/lib/jquery.fancybox.css');
 
     // Global scripts
     wp_enqueue_script( 'humblerootsmedia_script_jquery', get_template_directory_uri().'/js/lib/jquery-2.2.4.min.js' );
+    wp_enqueue_script( 'humblerootsmedia_script_fancybox', get_template_directory_uri().'/js/lib/jquery.fancybox.pack.js' );
     wp_enqueue_script( 'humblerootsmedia_script_smoothscroll', get_template_directory_uri() . '/js/lib/jquery.smooth-scroll.min.js' );
     wp_enqueue_script( 'humblerootsmedia_script', get_template_directory_uri().'/js/main.js' );
 
@@ -20,10 +22,8 @@ function humblerootsmedia_enqueue() {
      */
     if ( is_front_page() ) {
       wp_enqueue_style( 'humblerootsmedia_style_main', get_template_directory_uri().'/css/index.css' );
-      wp_enqueue_style( 'humblerootsmedia_style_fancybox', get_stylesheet_directory_uri() . '/css/lib/jquery.fancybox.css');
 
       wp_enqueue_script( 'humblerootsmedia_script_parallax', get_template_directory_uri().'/js/lib/jquery.parallax.min.js' );
-      wp_enqueue_script( 'humblerootsmedia_script_fancybox', get_template_directory_uri().'/js/lib/jquery.fancybox.pack.js' );
       wp_enqueue_script( 'humblerootsmedia_script_bxslider', get_template_directory_uri().'/js/lib/jquery.bxslider.min.js' );
       wp_enqueue_script( 'humblerootsmedia_script_instafeed', get_template_directory_uri().'/js/lib/instafeed.min.js' );
       wp_enqueue_script( 'humblerootsmedia_script_index', get_template_directory_uri().'/js/index.js' );
--- dev/wp-content/themes/humblerootsmedia/js/blog.js
+++ dev/wp-content/themes/humblerootsmedia/js/blog.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
       s.focus();
     }
   });
+
+  $('.post-content').find('img').each(function(index) {
+    $(this).wrap('<a class="fancybox" href=' + $(this).attr('src') + '></a>');
+  });
+
+  // Initialize FancyBox Modal
+  $(".fancybox")
+    .attr('rel', 'gallery')
+    .fancybox({
+        padding     : 0,
+        margin      : [20, 60, 20, 60] // Increase left/right margin
+    });
 });
