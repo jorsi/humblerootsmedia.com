6dc5eef9e81c0db50c4e694e3db49a555bd3fbb6
Jonathon Orsi <jonathon.orsi@gmail.com>
2016-06-28 11:46:36 -0400

    slight search bar adjustments

--- dev/wp-content/themes/humblerootsmedia/js/blog.js
+++ dev/wp-content/themes/humblerootsmedia/js/blog.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   submit.on('click', function(e) {
     if(!s.hasClass('open')) {
       e.preventDefault();
+      s.attr('placeholder', 'search...');
       s.toggleClass('open');
       s.focus();
     }
